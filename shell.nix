dc77b7adfa712fea4a43c12842ce973bbbc7b37c
phaer <hello@phaer.org>
2024-03-14 18:58:30 +0100

    Use github:phaer/nixpkgs/playwright

--- shell.nix
+++ shell.nix
@@ -1,7 +1,8 @@
 let
   nixpkgs = builtins.fetchTarball {
+    url = "https://github.com/phaer/nixpkgs/archive/a77a0eabcac6ba334fdecc66d4143d71984c6002.tar.gz";
     # https://status.nixos.org/ -> nixos-23.11 on 2024-03-12
-    url = "https://github.com/nixos/nixpkgs/archive/ddcd7598b2184008c97e6c9c6a21c5f37590b8d2.tar.gz";
+    #url = "https://github.com/nixos/nixpkgs/archive/ddcd7598b2184008c97e6c9c6a21c5f37590b8d2.tar.gz";
   };
   pkgs = import nixpkgs {};
 in
