496a38050f7076284b0fc40df0b68574d3779550
phaer <hello@phaer.org>
2024-03-18 19:56:19 +0100

    don't let playwright validate host requirements

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 let
   nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/phaer/nixpkgs/archive/a77a0eabcac6ba334fdecc66d4143d71984c6002.tar.gz";
+    url = "https://github.com/phaer/nixpkgs/archive/de966bde7c29589db944d71c94e1ecf5b3f64ee2.tar.gz";
     # https://status.nixos.org/ -> nixos-23.11 on 2024-03-12
     #url = "https://github.com/nixos/nixpkgs/archive/ddcd7598b2184008c97e6c9c6a21c5f37590b8d2.tar.gz";
   };
@@ -14,6 +14,7 @@ in
     ];
 
     shellHook = ''
+        export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
         export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
     '';
 
