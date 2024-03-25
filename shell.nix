afc76cc7b4014d8be201b31dc2aeae27523d4a62
phaer <hello@phaer.org>
2024-03-25 13:34:45 +0100

    fixed?

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 let
   nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/phaer/nixpkgs/archive/0784691d66852c4ca35b8d5cf39aa9b327ad28e1.tar.gz";
+    url = "https://github.com/phaer/nixpkgs/archive/de966bde7c29589db944d71c94e1ecf5b3f64ee2.tar.gz";
     # https://status.nixos.org/ -> nixos-23.11 on 2024-03-12
     #url = "https://github.com/nixos/nixpkgs/archive/ddcd7598b2184008c97e6c9c6a21c5f37590b8d2.tar.gz";
   };
@@ -14,7 +14,9 @@ in
     ];
 
     shellHook = ''
-        export DEBUG="pw:api,pw:install"
+        # Enable TLS support in Webkit, via GIO module.
+        export GIO_EXTRA_MODULES="${pkgs.glib-networking}/lib/gio/modules/:$GIO_EXTRA_MODULES}"
+        # Webkit misses libgles2 and gstreamer-libav.
         export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
         export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
     '';
