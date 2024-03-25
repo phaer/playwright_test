e39e4e7585a0b146ee5d1bf2d73a763b497dcb2f
phaer <hello@phaer.org>
2024-03-25 16:19:50 +0100

    moved GIO_EXTRA_MODULES to mkWrapper...
    ...also removed webkits gtk-based minibrowser,
    as only the WPE one is actually in use and it
    decreases closure size

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 let
   nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/phaer/nixpkgs/archive/de966bde7c29589db944d71c94e1ecf5b3f64ee2.tar.gz";
+    url = "https://github.com/phaer/nixpkgs/archive/7e754317e4652ca7f3196fbc149b7a19ccc088d7.tar.gz";
     # https://status.nixos.org/ -> nixos-23.11 on 2024-03-12
     #url = "https://github.com/nixos/nixpkgs/archive/ddcd7598b2184008c97e6c9c6a21c5f37590b8d2.tar.gz";
   };
@@ -14,9 +14,7 @@ in
     ];
 
     shellHook = ''
-        # Enable TLS support in Webkit, via GIO module.
-        export GIO_EXTRA_MODULES="${pkgs.glib-networking}/lib/gio/modules/:$GIO_EXTRA_MODULES}"
-        # Webkit misses libgles2 and gstreamer-libav.
+        # Webkit misses libgles2 and gstreamer-libav on ubuntu, but tests are passing
         export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
         export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
     '';
